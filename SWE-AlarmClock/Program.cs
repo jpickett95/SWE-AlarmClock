13f5377edb20f8440fdfac0990744ce623506c09
Jonah Pickett <86933257+jpickett95@users.noreply.github.com>
2022-07-22 13:38:18 -0400

    Add project files.

---
+++ SWE-AlarmClock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWE_AlarmClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
